4e25d90351e9b061891ca6b362a291062ab8f8ef
Lars, DK7LST <dk7lst@gmx.net>
2017-11-22 17:24:36 +0100

    - Fixed missing packages for Raspbian 9 (stretch). - Install and use external codec libs. - Update packages catalog before installing deps.

--- install-dep.sh
+++ install-dep.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # Pakete installieren gemäß "https://wiki.mumble.info/wiki/BuildingLinux#For_Debian_.2F_Ubuntu":
 # Zusätzlich für WiringPi-Lib auch git installieren.
+apt-get update
 apt-get install build-essential pkg-config qt4-dev-tools libqt4-dev libspeex1 \
                 libspeex-dev libboost-dev libasound2-dev libssl-dev g++ \
-                libspeechd-dev libzeroc-ice-dev ice-slice libpulse-dev slice2cpp \
+                libspeechd-dev libzeroc-ice-dev libpulse-dev \
                 libcap-dev libspeexdsp-dev libprotobuf-dev protobuf-compiler \
                 libogg-dev libavahi-compat-libdnssd-dev libsndfile1-dev \
-                libg15daemon-client-dev libxi-dev git-core
+                libg15daemon-client-dev libxi-dev git-core libopus-dev \
+                libcelt-dev
