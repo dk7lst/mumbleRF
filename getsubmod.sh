f88d7b93127a42a4a41b4027d243d07dcf42c956
Lars, DK7LST <dk7lst@gmx.net>
2017-12-18 17:26:24 +0100

    Switch to fixed codec versions needed. Should check licenses if possible to get rid of submodules as project should be independed of git magic.

--- getsubmod.sh
+++ getsubmod.sh
@@ -9,3 +9,18 @@ git clone https://git.xiph.org/celt.git celt-0.11.0-src
 git clone https://git.xiph.org/opus.git opus-src
 git clone https://github.com/mumble-voip/sbcelt.git sbcelt-src
 git clone https://github.com/mumble-voip/mach_override.git 3rdparty/mach-override-src
+
+# Switch to fixed versions needed:
+cd speex
+git checkout -q a6d05eb
+cd ../celt-0.7.0-src
+git checkout -q 6c79a93
+cd ../celt-0.11.0-src
+git checkout -q e3d39fe
+cd ../opus-src
+git checkout -q d060dd7
+cd ../sbcelt-src
+git checkout -q 045493d
+cd ../3rdparty/mach-override-src
+git checkout -q 919148f
+cd ../..
